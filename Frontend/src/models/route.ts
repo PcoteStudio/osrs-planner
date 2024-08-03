fe31ebd254861316823658736273c54c9332e766
Philippe Côté <cote.philippe@hotmail.com>
2024-08-02 22:26:57 -0400

    Implemented step execution in route

--- Frontend/src/models/route.ts
+++ Frontend/src/models/route.ts
@@ -1,20 +1,55 @@
-import { PlayerState } from "./playerState";
-import { Step } from "./step";
+import { PlayerState } from './playerState';
+import { Step } from './step';
 
 export class Route {
     playerState: PlayerState = new PlayerState();
-    firstStep: Step = new Step();
-    currentStep: Step | undefined = undefined;
-    lastStep: Step = this.firstStep;
+    firstStep: Step | undefined;
+    currentStep: Step | undefined; // Current step is considered already executed
+    lastStep: Step | undefined;
 
-    stepOnce() {
-        if (!this.currentStep) this.currentStep = this.firstStep;
-        this.firstStep.effect?.apply(this.playerState);
+    addStep(previousStep: Step | undefined, newStep: Step) {
+        if (newStep === undefined) return;
+
+        if (!previousStep) { // New step is first
+            const existingStep = this.firstStep;
+            this.firstStep = newStep;
+            newStep.next = existingStep;
+        } else {    // New step is not first
+            const existingStep = previousStep.next;
+            previousStep.next = newStep;
+            newStep.next = existingStep;
+        }
+
+        if (newStep.next === undefined) // New step is last
+            this.lastStep = newStep;
+        else // New step is not last
+            newStep.next.previous = newStep;
     }
 
+    /**
+     * Applies the next step.
+     * @returns `true` if a another step was executed or `false` otherwise.
+     */
+    stepOnce(): boolean {
+        if (!this.currentStep && this.firstStep)
+            this.currentStep = this.firstStep;
+        else if (this.currentStep?.next)
+            this.currentStep = this.currentStep.next;
+        else
+            return false;
+        this.currentStep.applyEffects(this.playerState);
+        return true;
+    }
+
+    /**
+     * Applies the next steps until the specified step is applied or until the last step.
+     * @param step Once this step is executed, will return.
+     */
     stepUntil(step: Step) {
-        do {
-            this.stepOnce();
-        } while (step.id !== this.currentStep?.id && this.lastStep.id !== this.currentStep?.id);
+        let wasStepExecuted = true;
+        while (step.id !== this.currentStep?.id && wasStepExecuted) {
+            wasStepExecuted = this.stepOnce();
+        }
     }
+
 }
--- Frontend/src/models/step.ts
+++ Frontend/src/models/step.ts
@@ -1,11 +1,18 @@
 import { nanoid } from 'nanoid';
-import type { Effect } from "./effect";
+import type { Effect } from './effect';
+import type { PlayerState } from './playerState';
 
 export class Step {
     id: string = nanoid();
     depth: number = 0;
     description: string = '';
-    effect: Effect | undefined;
-    previousStep: Step | undefined;
-    nextStep: Step | undefined;
+    effects: Effect[] = [];
+    previous: Step | undefined;
+    next: Step | undefined;
+
+    applyEffects(playerState: PlayerState) {
+        for (const effect of this.effects) {
+            effect.apply(playerState);
+        }
+    }
 }
