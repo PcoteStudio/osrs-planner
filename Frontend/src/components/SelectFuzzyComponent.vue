7cb772bb6a5735c3b64897eeaea607a294559971
woofi7 <n.signo@hotmail.com>
2024-09-03 23:03:32 -0400

    We got a fuzzy select!!!

--- Frontend/src/components/SelectFuzzyComponent.vue
+++ Frontend/src/components/SelectFuzzyComponent.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
 import { useGlobalStore } from '@/stores/globalStore';
-import { computed, ref, watch } from 'vue';
+import { computed, ref } from 'vue';
 import { ItemStore } from '@/models/item/itemStore';
 import * as fuzzySort from 'fuzzysort';
 import { Item } from '@/models/item/item';
-import KeysResults = Fuzzysort.KeysResults;
 import KeysResult = Fuzzysort.KeysResult;
-import type { ComputedRef } from '@vue/reactivity';
-import _, { debounce } from 'lodash';
+import _ from 'lodash';
 import { watchDebounced } from '@vueuse/core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 const store = useGlobalStore();
+
+type HighlightResult = Array<{match: string} | string>;
+type SearchResultElement = (KeysResult<Item> | {obj: Item}) & {highlightedName: HighlightResult}
+
+const filter = ref('');
+const filteredData = ref();
+const op = ref();
+const input = ref();
+const width = ref();
+const selectedItem = ref();
+const active = ref(false);
 
 const data = computed(() => {
   return Object.values(ItemStore.items)
@@ -21,11 +31,6 @@ const data = computed(() => {
 const fuzzySearchKeys = [
   'name',
 ];
-const filter = ref();
-
-type HighlightResult = Array<{match: string} | string>;
-type SearchResultElement = (KeysResult<Item> | {obj: Item}) & {highlightedName: HighlightResult}
-
 const highlight = (result: KeysResult<Item>, key: string): HighlightResult => {
   const highlightResult  = result[fuzzySearchKeys.indexOf(key)]
     .highlight((m: string, _i: number) => ({ match: m }));
@@ -35,54 +40,97 @@ const highlight = (result: KeysResult<Item>, key: string): HighlightResult => {
   return [_.get(result.obj, key)];
 };
 
-const filteredData = ref();
-
-
 watchDebounced((filter), () => {
   let result: Array<SearchResultElement> = fuzzySort.go(
     filter.value,
     data.value,
-    { keys: fuzzySearchKeys, all: true, limit: 100 }
+    { keys: fuzzySearchKeys, all: true, limit: 100 },
   ).map(result => ({
     ...result,
-    highlightedName: highlight(result, 'name')
+    highlightedName: highlight(result, 'name'),
   }));
   let filterAsId = +filter.value;
   let itemSameId = data.value.find(i => i.id === filterAsId);
   if (itemSameId) {
     result.unshift({
       obj: itemSameId,
-      highlightedName: [itemSameId.name]
+      highlightedName: [itemSameId.name],
     });
   }
   filteredData.value = result;
-}, { debounce: 300 });
+}, { debounce: 300, immediate: true });
 
+const togglePopup = (event?: FocusEvent) => {
+  if (event?.type === 'focusin') {
+    width.value = input.value.$el.offsetWidth;
+    op.value.show(event);
+  }
+  else {
+    op.value?.hide(event);
+    active.value = false;
+  }
+};
 
-const op = ref();
-const input = ref();
-const width = ref();
-const togglePopup = (event: FocusEvent) => {
-  width.value = input.value.$el.offsetWidth;
-  op.value.toggle(event);
+const setSelectedItem = (item: Item) => {
+  selectedItem.value = item;
+  filter.value = item.name;
+  togglePopup();
+};
+
+const setActive = () => {
+  active.value = true;
+  setTimeout(() => {
+    input.value.$el.focus();
+  }, 0);
+};
+
+const clearFilter = () => {
+  filter.value = '';
+  console.log(filter.value);
+  setTimeout(() => {
+    input.value.$el.focus();
+  }, 0);
 };
 </script>
 
 <template>
   <div class="select">
-    <InputText
-        ref="input"
-        class="search"
-        v-model="filter"
-        type="text"
-        @focus="togglePopup"
-    />
-    <Popover ref="op" unstyled="false" :style="{ width: width + 'px' }">
+    <FloatLabel>
+      <div :style="{display: active ? 'block' : 'none', position: 'relative' }">
+        <InputText
+            ref="input"
+            class="search"
+            v-model="filter"
+            type="text"
+            name="search-box"
+            @focusin="togglePopup"
+        />
+      </div>
+      <div v-if="!active"
+           @click="setActive"
+           class="search-placeholder p-inputtext cursor-pointer"
+      >
+        <img :src="selectedItem?.imageUrl"  :alt="selectedItem?.name"/>
+        <span class="name">
+          {{ selectedItem?.name }}
+        </span>
+      </div>
+      <div v-if="active" class="clear" @click="clearFilter">
+        <font-awesome-icon icon="x" class="label" />
+      </div>
+      <div v-else class="clear" @click="setActive">
+        <font-awesome-icon icon="chevron-down" class="label" />
+      </div>
+      <label for="search-box">Item</label>
+    </FloatLabel>
+    <Popover ref="op" :unstyled="true" :style="{ width: width + 'px' }" >
       <div v-if="filteredData" class="list">
         <div
             v-for="(matchedResults, index) in filteredData"
             :key="index"
             class="item"
+            @pointerdown.prevent
+            @click="() => setSelectedItem(matchedResults.obj)"
         >
           <img :src="matchedResults.obj.imageUrl"  :alt="matchedResults.obj.name"/>
           <div class="name">
@@ -92,6 +140,10 @@ const togglePopup = (event: FocusEvent) => {
             </template>
           </div>
           <span v-if="matchedResults.obj.id.toString() === filter" class="highlight-match">{{ matchedResults.obj.id }}</span>
+        </div>
+        <div  v-if="filteredData.length === 0" class="item disabled">
+          <font-awesome-icon icon="face-sad-cry" style="height: 20px" class="p-1"/>
+          <span>No item found</span>
         </div>
       </div>
     </Popover>
@@ -103,6 +155,35 @@ const togglePopup = (event: FocusEvent) => {
   position: relative;
   display: flex;
   flex-direction: column;
+
+  .search {
+    width: 100%;
+  }
+}
+
+.clear {
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 1em;
+  color: #a1a1aa;
+
+  .label:hover {
+    color: white;
+    cursor: pointer;
+  }
+}
+
+.search-placeholder {
+  height: 2.75em;
+  display: flex;
+  flex-direction: row;
+  gap: 0.5em;
 }
 .list {
   background-color: #18181b;
@@ -122,6 +203,12 @@ const togglePopup = (event: FocusEvent) => {
     align-items: center;
     gap: 0.5em;
     padding: 0.2em 1em;
+    
+    &.disabled:hover {
+      background-color: initial;
+      cursor: initial;
+      user-select: none;
+    }
 
     &:hover {
       background-color: #27272a;
